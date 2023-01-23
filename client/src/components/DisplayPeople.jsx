bec6569c58664bfa95a04b5c8407e073463b1dcd
HiImWhite <wojttasek1999@gmail.com>
2023-01-23 18:12:22 +0100

    Reworking component

--- client/src/components/DisplayPeople.jsx
+++ client/src/components/DisplayPeople.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
-function DisplayPeople(props) {
+import InfoModal from './InfoModal';
+
+function DisplayPeople({ characters, loader }) {
+  const [displayModal, setDisplayModal] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const selectPerson = (person) => {
+    setDisplayModal(true);
+    setSelectedCharacter(person);
+  };
+
+  const handleCloseModal = () => setDisplayModal(false);
+
   return (
-    <div>
-      {props.people.length > 0 && (
-        <>
-          <h2>Characters</h2>
-          {props.people.map((person) => (
-            <div key={person.name}>
-              <p>Name: {person.name}</p>
-            </div>
-          ))}
-        </>
-      )}
-      {props.residents.length > 0 && (
-        <>
-          <h2>Residents</h2>
-          {props.residents.map((resident) => (
-            <div key={resident.name}>
-              <p>Name: {resident.name}</p>
-            </div>
-          ))}
-        </>
-      )}
-    </div>
+    <Box height='90%' overflow='auto'>
+      <Typography variant='h5'>Characters</Typography>
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        flexDirection='column'
+        height='fit-content'>
+        {characters.length > 0 ? (
+          <Box display='flex' flexDirection='column' gap='0.5rem'>
+            {characters.map((person) => (
+              <Box key={person.name}>
+                <Typography
+                  onClick={() => selectPerson(person)}
+                  sx={{ cursor: 'pointer' }}>
+                  Name: {person.name}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+        ) : loader ? (
+          <CircularProgress />
+        ) : (
+          <Typography>No data</Typography>
+        )}
+      </Box>
+      <InfoModal
+        handleOpen={displayModal}
+        selectedCharacter={selectedCharacter}
+        handleCloseModal={handleCloseModal}
+      />
+    </Box>
   );
 }
 
