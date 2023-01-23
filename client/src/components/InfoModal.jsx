d413dd42109b59a610333af1ce1ac3ec3bcf0421
HiImWhite <wojttasek1999@gmail.com>
2023-01-23 19:10:16 +0100

    Getting film info and styling

--- client/src/components/InfoModal.jsx
+++ client/src/components/InfoModal.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import CloseIcon from '@mui/icons-material/Close';
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
@@ -13,21 +15,49 @@ const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
+  borderRadius: 2,
 };
 
 function InfoModal({ handleOpen, selectedCharacter, handleCloseModal }) {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    if (selectedCharacter) {
+      const fetchFilms = async () => {
+        const filmPromises = selectedCharacter.films.map((filmUrl) =>
+          axios.get(filmUrl),
+        );
+        const filmResponses = await Promise.all(filmPromises);
+        setFilms(filmResponses.map((response) => response.data));
+      };
+      fetchFilms();
+    }
+  }, [selectedCharacter]);
+
   return (
     <Modal open={handleOpen} onClose={handleCloseModal}>
       <Box sx={style}>
         {selectedCharacter && (
           <>
+            <CloseIcon
+              onClick={handleCloseModal}
+              sx={{
+                cursor: 'pointer',
+                float: 'right',
+                width: '30px',
+              }}
+            />
             <Typography>Name: {selectedCharacter.name}</Typography>
             <Typography>Films:</Typography>
             <Box display='flex' flexDirection='column' gap='0.5rem'>
-              {selectedCharacter.films.map((film, index) => (
-                <Typography sx={{ cursor: 'pointer' }} key={index}>
-                  {film}
-                </Typography>
+              {films.map((film) => (
+                <Box key={film.title}>
+                  <Typography>Title: {film.title}</Typography>
+                  <Typography>Release Date: {film.release_date}</Typography>
+                  <Typography>
+                    Opening Crawl: {film.opening_crawl.slice(0, 130) + '...'}
+                  </Typography>
+                </Box>
               ))}
             </Box>
           </>
